bf4e9c1f1d866be6243e75a7853dae5cb814ef76
abhi120785 <56971911+abhi120785@users.noreply.github.com>
2020-07-06 16:19:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
-<?php
-
-echo "Hello World! . Staging is here";
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Swap with Preview test</title>
+<h1>Swap with Preview Test Page</h1>
+<table>
+<tbody>
+<tr>
+<th>Server Variable</th>
+<th>Value</th>
+</tr>
+<tr>
+<td>Process ID:</td>
+<td><%= System.Diagnostics.Process.GetCurrentProcess().Id %></td>
+</tr>
+<tr>
+<td>SlotName:</td>
+<td><%= Environment.GetEnvironmentVariable("SlotName") %></td>
+</tr>
+<tr>
+<td>ConnectionString:</td>
+<td><%= Environment.GetEnvironmentVariable("SQLAZURECONNSTR_ConnectionString") %></td>
+</tr>
+</tbody>
+</table>
 
